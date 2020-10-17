2a2d919fdc1c0986aadce8aea4324af7b6e58f45
DeborahCox <280557445@qq.com>
2020-10-18 05:43:39 +0800

    Update chase.js

--- CSS/addons/chase.js
+++ CSS/addons/chase.js
@@ -13,7 +13,7 @@ $(document).mousemove(function(event) {
     windowHeight = $(window).height();
     
     mouseXpercentage = Math.round(event.pageX / 210 * 50);
-    mouseYpercentage = Math.round(event.pageY / windowHeight * 50);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 10);
     
   $('.PageSidebar\\/sidebarContainer\\/3Hxpq').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
   });
