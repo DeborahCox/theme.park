9d99b642262e07c2082be899cac80288e0b09dbb
DeborahCox <280557445@qq.com>
2020-10-18 05:28:44 +0800

    sidebar fixed

--- CSS/addons/chase.js
+++ CSS/addons/chase.js
@@ -6,5 +6,14 @@ $(document).mousemove(function(event) {
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
     
     $('.root').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
-    $('.PageSidebar\\/sidebarContainer\\/3Hxpq').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+  });
+
+$(document).mousemove(function(event) {
+    windowWidth = $(window).width();
+    windowHeight = $(window).height();
+    
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    
+  $('.PageSidebar\\/sidebarContainer\\/3Hxpq').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
   });
