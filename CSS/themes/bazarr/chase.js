a8c98ad38cf783a23d027b6e153e9b6fe0117567
DeborahCox <280557445@qq.com>
2020-10-22 20:41:51 +0800

    add more chase

--- CSS/themes/bazarr/chase.js
+++ CSS/themes/bazarr/chase.js
@@ -1,10 +1,9 @@
 $(document).mousemove(function(event) {
     windowWidth = $(window).width();
     windowHeight = $(window).height();
-    
+
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    
-    $('.root').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+
+    $('.fix-header').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #47918a, #0b3161)');
   });
-  
